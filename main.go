f8efc0d24ff7aed28a83cd9e11055326eaa7396d
Mayankkumar21 <mayankyadav25@gmail.com>
2023-03-25 18:38:43 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+	var bingDomains = map[string]string{
+	"com": "",
+	"uk": "&cc=GB",
+	"us": "&cc=US",
+	"tr": "&cc=TR",
+	"tw": "&cc=TW",
+	"ch": "&cc=CH",
+	"se": "&cc=SE",
+	"es": "&cc=ES",
+	"za": "&cc=ZA",
+	"sa": "&cc=SA",
+	"ru": "&cc=RU",
+	"ph": "&cc=PH",
+	"pt": "&cc=PT",
+	"pl": "&cc=PL",
+	"cn": "&cc=CN",
+	"no": "&cc=NO",
+	"nz": "&cc=NZ",
+	"nl": "&cc=NL",
+	"mx": "&cc=MX",
+	"my": "&cc=MY",
+	"kr": "&cc=KR",
+	"jp": "&cc=JP",
+	"it": "&cc=IT",
+	"id": "&cc=ID",
+	"in": "&cc=IN",
+	"hk": "&cc=HK",
+	"de": "&cc=DE",
+	"fr": "&cc=FR",
+	"fi": "&cc=FI",
+	"dk": "&cc=DK",
+	"cl": "&cc=CL",
+	"ca": "&cc=CA",
+	"br": "&cc=BR",
+	"be": "&cc=BE",
+	"at": "&cc=AT",
+	"au": "&cc=AU",
+	"ar": "&cc=AR",
+}
+
+type searchResult struct{
+	ResultRank int;
+	ResultURL string;
+	ResultTitle string;
+	ResultDesc string;
+}
+var userAgents = []string{
+
+}
+
+func randomUserAgent()string{
+
+}
+func buildUrl()string{
+
+}
